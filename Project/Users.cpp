c402e4dfcbdd1c632d5c7422037ce4ef530750dd
delse <zaharov.sasha.02@gmail.com>
2021-06-07 21:14:27 +0300

    Добавление авторизации пользователей

--- Project/Users.cpp
+++ Project/Users.cpp
@@ -1,1 +1,15 @@
 #include "Users.h"
+
+bool User::LogIn(vector <UserData> userInformation) {
+	cout << "Enter login:";
+	cin >> login;
+	cout << "Enter password:";
+	cin >> password;
+
+	for (UserData info : userInformation) {
+		if (!info.login.compare(login) && !info.password.compare(password)) {
+			return true;
+		}
+	}
+	return false;
+}
